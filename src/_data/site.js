5dd58f1b80e3fdbd163522efbdc5f8c6266da364
Tai <bvt.vantai@gmail.com>
2026-03-12 22:24:59 +0700

    Updates

--- src/_data/site.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: "AiOps Bridge",
-  url: process.env.SITE_URL || "https://aiopsbridge.com",
-  googleVerification: process.env.GOOGLE_SITE_VERIFICATION || "",
-  preview: process.env.PREVIEW === "true"
-};
